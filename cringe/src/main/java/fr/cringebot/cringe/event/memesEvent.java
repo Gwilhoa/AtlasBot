4c08c8b5d78cf8a0ae0db25aa58aa04e6c81296a
gchatain <guilheimchataing@gmail.com>
2021-11-29 17:44:54 +0100

    modification des memes fix
    Former-commit-id: b72e5f34d6d718b70331dcd4f258d2f9855bfbff

--- cringe/src/main/java/fr/cringebot/cringe/event/memesEvent.java
+++ cringe/src/main/java/fr/cringebot/cringe/event/memesEvent.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.api.entities.MessageActivity;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.MessageReaction;
 import net.dv8tion.jda.api.requests.restaction.MessageAction;
+import net.dv8tion.jda.internal.requests.restaction.MessageActionImpl;
 
 import java.awt.*;
 import java.io.File;
@@ -30,12 +31,21 @@ public class memesEvent {
         int nb = u-d;
         if (nb <= -5)
             message.delete().queue();
-        else if (nb >= 5) {
+        else if (nb >= 1) {
+            ArrayList<Message.Attachment> attachments = new ArrayList<>(message.getAttachments());
             if ((message.getAttachments().size() >= 1 && message.getAttachments().get(0).isImage()) || (message.getAttachments().size() >= 1 && message.getAttachments().get(0).isVideo())) {
-                MessageAction msg = message.getGuild().getTextChannelById("911549374696411156").sendMessage(message.getContentRaw());
-                for ( Message.Attachment ac : message.getAttachments()) {
+                MessageAction msg;
+                if (!message.getContentRaw().isEmpty())
+                    msg = message.getGuild().getTextChannelById("911549374696411156").sendMessage(message.getContentRaw());
+                else {
+                    File f = attachments.get(0).downloadToFile(message.getAttachments().get(0).getFileName()).join();
+                    msg = message.getGuild().getTextChannelById("911549374696411156").sendFile(f);
+                    f.deleteOnExit();
+                    attachments.remove(0);
+                }
+                for ( Message.Attachment ac : attachments) {
                     File file = ac.downloadToFile(ac.getFileName()).join();
-                    msg = msg.addFile(file);
+                    msg.addFile(file);
                     file.deleteOnExit();
                 }
                 msg.queue();
@@ -43,7 +53,7 @@ public class memesEvent {
             else if (UtilFunction.isImage(message.getContentRaw()) || UtilFunction.isTwitter(message.getContentRaw()) || UtilFunction.isVideo(message.getContentRaw()) || UtilFunction.isYoutube(message.getContentRaw())) {
                 message.getGuild().getTextChannelById("911549374696411156").sendMessage(message.getContentRaw()).queue();
             }
-                message.delete().queue();
+            message.delete().queue();
         }
     }
 }
