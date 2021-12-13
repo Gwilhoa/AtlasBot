d25fb4d5a9611d8f1280aa65b575bda2faa760b2
Gwilhoa <guilheimchataing@gmail.com>
2021-12-13 15:18:37 +0100

    rework git + meme event
    Former-commit-id: b12d14526aba0d49858582c882ae91d1be1bb037

---
+++ src/main/java/fr/cringebot/cringe/objects/activity.java
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   activity.java                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gchatain <gchatain@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/05 13:05:12 by gchatain          #+#    #+#             */
+/*   Updated: 2021/12/13 00:57:12 by gchatain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+package fr.cringebot.cringe.objects;
+
+import net.dv8tion.jda.internal.entities.ActivityImpl;
+
+/**
+ * sert à changer l'activité du bot
+ */
+public class activity extends ActivityImpl {
+
+    public activity(String name) {
+        super(name);
+    }
+
+    public activity(String name, String url) {
+        super(name, url);
+    }
+
+    public activity(String name, String url, ActivityType type) {
+        super(name, url, type);
+    }
+
+    public activity(String name, String url, ActivityType type, Timestamps timestamps, Emoji emoji) {
+        super(name, url, type, timestamps, emoji);
+    }
+}
