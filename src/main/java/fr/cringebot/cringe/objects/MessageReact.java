3077a6aa42a86525a37bccb0c4027f63611ccf79
gchatain <gchatain@student.42lyon.fr>
2022-02-12 21:16:11 +0100

    oui
    Former-commit-id: c51abe4464707a12245298263bd862300161fc14

---
+++ src/main/java/fr/cringebot/cringe/objects/MessageReact.java
@@ -0,0 +1,97 @@
+package fr.cringebot.cringe.objects;
+
+import com.google.gson.reflect.TypeToken;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
+
+import java.awt.*;
+import java.io.*;
+import java.util.ArrayList;
+
+import static fr.cringebot.cringe.event.BotListener.gson;
+
+public class MessageReact {
+    private static final String channel = "853210283480055809";
+
+    private static final String file = "save/MessageReaction.json";
+    private static final TypeToken<ArrayList<MessageReact>> typeToken = new TypeToken<ArrayList<MessageReact>>() {
+    };
+    public static ArrayList<MessageReact> message;
+    private final String Title;
+    private final String Id;
+    public ArrayList<RoleReaction> list;
+
+
+    public MessageReact(String title, Guild guild) {
+        load();
+        list = new ArrayList<>();
+        Title = title;
+        Id = guild.getTextChannelById(channel).sendMessageEmbeds(new EmbedBuilder().setColor(new Color(138, 43, 226)).setTitle(title).build()).complete().getId();
+        message.add(this);
+        save();
+    }
+
+    public static void load() {
+        if (new File(file).exists()) {
+            try {
+                message = gson.fromJson(new BufferedReader(new InputStreamReader(new FileInputStream(file))), typeToken.getType());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                new File(file).createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (message == null)
+            message = new ArrayList<>();
+    }
+
+    public static void save() {
+        if (!new File(file).exists()) {
+            try {
+                new File(file).createNewFile();
+            } catch (IOException e) {
+                return;
+            }
+        }
+        try {
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+            gson.toJson(message, typeToken.getType(), bw);
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+        }
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void addRole(RoleReaction rr, Guild guild) {
+        this.list.add(rr);
+        save();
+        refresh(guild);
+    }
+
+    public void refresh(Guild guild) {
+        Message msg = guild.getTextChannelById(channel).retrieveMessageById(this.Id).complete();
+        StringBuilder sb = new StringBuilder();
+        for (RoleReaction rr : this.list) {
+            rr.verif(guild.getTextChannelById(channel).retrieveMessageById(this.Id).complete());
+            sb.append(rr.getName()).append(" ").append(rr.getEmote()).append('\n');
+            msg.addReaction(rr.getEmote()).queue();
+        }
+        EmbedBuilder eb = new EmbedBuilder().setTitle(this.Title).setColor(msg.getEmbeds().get(0).getColor());
+        msg.editMessageEmbeds(eb.setDescription(sb.toString()).build()).queue();
+
+    }
+}
+
---
+++ src/main/java/fr/cringebot/cringe/objects/RoleReaction.java
@@ -0,0 +1,53 @@
+package fr.cringebot.cringe.objects;
+
+import net.dv8tion.jda.api.entities.*;
+
+import java.util.List;
+
+public class RoleReaction {
+    private String Name;
+    private final String Id;
+    private final String Emote;
+
+    public RoleReaction(String name, String roleId, String emote) {
+        this.Name = name;
+        this.Id = roleId;
+        this.Emote = emote;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+
+    public String getEmote() {
+        return Emote;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void verif(Message msg)
+    {
+        List<User> userList = null;
+        this.setName(msg.getGuild().getRoleById(this.getId()).getName());
+        for (MessageReaction mr : msg.getReactions()) {
+            if (mr.getReactionEmote().getAsReactionCode().equals(this.getEmote())) {
+                userList = mr.retrieveUsers().complete();
+                for (User user : userList)
+                    if (!msg.getGuild().getMemberById(user.getId()).getRoles().contains(msg.getGuild().getRoleById(this.getId())))
+                        msg.getGuild().addRoleToMember(user.getId(),msg.getGuild().getRoleById(this.getId())).queue();
+                    break;
+            }
+        }
+        for (Member mem : msg.getGuild().getMembersWithRoles(msg.getGuild().getRoleById(this.getId())))
+        {
+            if (!userList.contains(mem.getUser()))
+                msg.getGuild().removeRoleFromMember(mem, msg.getGuild().getRoleById(this.getId())).queue();
+        }
+    }
+}
