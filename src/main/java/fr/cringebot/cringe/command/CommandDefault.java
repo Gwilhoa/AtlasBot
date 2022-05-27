e2d186e25ace0a15358ed71e1cc876062b661840
gchatain <gchatain@student.42.fr>
2022-05-27 13:17:02 +0200

    test problem

--- src/main/java/fr/cringebot/cringe/command/CommandDefault.java
+++ src/main/java/fr/cringebot/cringe/command/CommandDefault.java
@@ -99,9 +99,9 @@ public class CommandDefault {
 	private void top(Message msg){
 		List<Squads> squads = Squads.getAllSquads();
 		StringBuilder sb = new StringBuilder();
-		sb.append(squads.get(0).getName()).append(" ").append(squads.get(0).getTotal()).append(" ").append(msg.getGuild().getMemberById(squads.get(0).getBestid()));
-		sb.append(squads.get(1).getName()).append(" ").append(squads.get(1).getTotal()).append(" ").append(msg.getGuild().getMemberById(squads.get(1).getBestid()));
-		sb.append(squads.get(2).getName()).append(" ").append(squads.get(2).getTotal()).append(" ").append(msg.getGuild().getMemberById(squads.get(2).getBestid()));
+		sb.append(squads.get(0).getName()).append(" ").append(squads.get(0).getTotal()).append(" ").append(msg.getGuild().getMemberById(squads.get(0).getBestid()).getAsMention());
+		sb.append(squads.get(1).getName()).append(" ").append(squads.get(1).getTotal()).append(" ").append(msg.getGuild().getMemberById(squads.get(1).getBestid()).getAsMention());
+		sb.append(squads.get(2).getName()).append(" ").append(squads.get(2).getTotal()).append(" ").append(msg.getGuild().getMemberById(squads.get(2).getBestid()).getAsMention());
 
 		EmbedBuilder eb = new EmbedBuilder().setTitle("Classement :");
 		eb.setDescription(sb);
@@ -160,7 +160,10 @@ public class CommandDefault {
 			msg.getChannel().sendMessage("le bot est actuellement en maintenance").queue();
 			return;
 		}
-		WaifuCommand.CommandMain(msg);
+		if (msg.getMember().getId().equals("420655502276558848"))
+			WaifuCommand.CommandMain(msg);
+		else
+			msg.getChannel().sendMessage("aparemment j'ai un probleme, je suis actuellement en train d'etre examiné").queue();
 	}
 
 	@Command(name = "cki", description = "mais qui est-il !", type = ExecutorType.USER)
