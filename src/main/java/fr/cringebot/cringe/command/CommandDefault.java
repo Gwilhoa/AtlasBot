b618f2ae414000671c146c824ba9800a86fd70b7
gchatain <guilheimchataing@gmail.com>
2022-05-27 22:53:07 +0200

    test vocal xp + command rank

--- src/main/java/fr/cringebot/cringe/command/CommandDefault.java
+++ src/main/java/fr/cringebot/cringe/command/CommandDefault.java
@@ -6,7 +6,7 @@
 /*   By: gchatain <gchatain@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 12:46:08 by gchatain          #+#    #+#             */
-/*   Updated: 2022/05/27 01:24:00 by                  ###   ########.fr       */
+/*   Updated: 2022/05/27 22:52:01 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,9 +99,9 @@ public class CommandDefault {
 	private void top(Message msg){
 		List<Squads> squads = Squads.getAllSquads();
 		StringBuilder sb = new StringBuilder();
-		sb.append(squads.get(0).getName()).append(" ").append(squads.get(0).getTotal()).append(" meilleur : ").append(msg.getGuild().getMemberById(squads.get(0).getBestid()).getAsMention()).append("\n");
-		sb.append(squads.get(1).getName()).append(" ").append(squads.get(1).getTotal()).append(" meilleur : ").append(msg.getGuild().getMemberById(squads.get(1).getBestid()).getAsMention()).append("\n");
-		sb.append(squads.get(2).getName()).append(" ").append(squads.get(2).getTotal()).append(" meilleur : ").append(msg.getGuild().getMemberById(squads.get(2).getBestid()).getAsMention());
+		sb.append(squads.get(0).getName()).append(" ").append(squads.get(0).getTotal()).append(" ").append(msg.getGuild().getMemberById(squads.get(0).getBestid()));
+		sb.append(squads.get(1).getName()).append(" ").append(squads.get(1).getTotal()).append(" ").append(msg.getGuild().getMemberById(squads.get(1).getBestid()));
+		sb.append(squads.get(2).getName()).append(" ").append(squads.get(2).getTotal()).append(" ").append(msg.getGuild().getMemberById(squads.get(2).getBestid()));
 
 		EmbedBuilder eb = new EmbedBuilder().setTitle("Classement :");
 		eb.setDescription(sb);
@@ -183,12 +183,9 @@ public class CommandDefault {
 	private void test(Message msg) throws IOException {
 		if (msg.getMember().getPermissions().contains(Permission.ADMINISTRATOR))
 		{
-			ArrayList<waifu> waifus = waifu.getAllWaifu();
-			for (waifu waifu : waifus)
-			{
-				if (waifu.getOwner() != null && waifu.getOwner().equals("420655502276558848"))
-					waifu.setOwner(null);
-			}
+			new Squads(msg.getGuild().getRoleById("979455621638262784"));
+			new Squads(msg.getGuild().getRoleById("979455622145773589"));
+			new Squads(msg.getGuild().getRoleById("979455623295041578"));
 		}
 	}
 }
